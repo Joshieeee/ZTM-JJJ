a4a1ddbdb2154128053a01436ab500474a2af0e2
Josh <51180404+Joshieeee@users.noreply.github.com>
2021-01-13 17:19:14 -0800

    Add files via upload
    Completed JS files.

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 var map;
 var mapArray = [];
+var trailNumber = 0;
+var randomHike = "";
+var userLat = 0;
+var userLng = 0;
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    userLat = position.coords.latitude;
+    userLng = position.coords.longitude;
+    console.log(userLat, userLng);
+    createMap();
+}
+
+getLocation();
 
 function createMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 45.5811,
-            lng: -122.3538
+            lat: userLat,
+            lng: userLng,
         },
         zoom: 10
     });
@@ -24,13 +47,19 @@ function createMap() {
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results.length);
+        trailNumber = results.length;
         for (var i = 0; i < results.length; i++) {
             createMarker(results[i]);
             mapArray[i] = results[i].name;
             console.log(mapArray[i]);
         }
     }
+
+}
+
+function randomTrail() {
+    randomHike = mapArray[Math.floor(Math.random() * Math.floor(trailNumber))];
+    document.getElementById('hikingTrail').value = randomHike;
 }
 
 function createMarker(place) {
@@ -41,3 +70,5 @@ function createMarker(place) {
         title: place.name
     })
 }
+
+document.getElementById("hikeButton").addEventListener("click", randomTrail.bind());
