f50ac00ee98e4f418b6fa03ca4ced0f67bfc3067
Josh <51180404+Joshieeee@users.noreply.github.com>
2021-01-14 16:18:19 -0800

    Add files via upload
    Adding BootStrap/CSS files.

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 var map;
 var mapArray = [];
+var trailNumber = 0;
+var randomHike = "";
+var userLat = 0;
+var userLng = 0;
+var trailFound = false;
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    userLat = position.coords.latitude;
+    userLng = position.coords.longitude;
+    console.log(userLat, userLng);
+    
+    createMap();
+}
+
+getLocation();
 
 function createMap() {
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 45.5811,
-            lng: -122.3538
+            lat: userLat,
+            lng: userLng,
         },
         zoom: 10
     });
+}
 
+function findTrails() {
     var request = {
         location: map.getCenter(),
-        radius: 80047,
+        radius: document.getElementById("searchRadius").value * 1609,
         // types: ['campground'],
-        keyword: ['trails']
+        keyword: "(trails) or (trailhead) or (trail)" //DOES NOT WORK, FIND FIX!
     }
 
     var service = new google.maps.places.PlacesService(map);
@@ -24,20 +50,44 @@ function createMap() {
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log(results.length);
+        trailNumber = results.length;
         for (var i = 0; i < results.length; i++) {
             createMarker(results[i]);
-            mapArray[i] = results[i].name;
-            console.log(mapArray[i]);
+            mapArray[i] = results[i];
+            // console.log(mapArray[i]);
         }
     }
 }
 
+function randomTrail() {
+    randomHike = mapArray[Math.floor(Math.random() * Math.floor(trailNumber))];
+    document.getElementById('hikingTrail').value = randomHike.name;
+    trailFound = true;
+    createMarker(randomHike);
+}
+
 function createMarker(place) {
+
+    var hikingIcon = {
+        url: 'https://www.campgroundsigns.com/img/lg/X/mutcd-campground-guide-sign-hiking-trail-x-rs-068.png',
+        scaledSize: trailFound ? new google.maps.Size(50,50) : new google.maps.Size(25,25)
+        
+    }
+
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location,
-        title: place.name
+        title: place.name,
+        icon: hikingIcon
     })
+
+    console.log()
 }
+
+document.getElementById("hikeButton").addEventListener("click", randomTrail.bind());
+document.getElementById("searchRadius").addEventListener("keypress", function(e) {
+    if(e.key === "Enter"){
+        findTrails();
+    }
+})
